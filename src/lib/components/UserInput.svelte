2d7576599588b979f2841f8ebcac9f5744d3be99
rskriver <77011442+rskriver@users.noreply.github.com>
2024-10-19 19:21:53 +0200

    first commit

---
+++ src/lib/components/UserInput.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface UserInputInterface {
+		children: Snippet;
+		userName: string;
+	}
+	let { userName, children, ...props }: UserInputInterface = $props();
+	$inspect(children, 'data');
+
+	// let userName = $state('');
+	$inspect(userName, 'userName');
+</script>
+
+<h1>Your username is: {userName}</h1>
+<input type="text" bind:value={userName} placeholder="Your Username" />
+{@render children()}
+
+<p></p>
+
+<style>
+	* {
+		font-size: 2rem;
+		display: flex;
+		justify-content: center;
+		/* text-align: center;
+        align-items: center;
+        align-content: center; */
+		margin: auto;
+		padding: 20px;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { sound } from 'svelte-sound';
+	import click_sound from '../lib/assets/mixkit-hard-typewriter-click-1119.wav';
+	import hover_sound from '../lib/assets/mixkit-helicopter-flying-far-away-2715.wav';
+	import UserInput from '../lib/components/UserInput.svelte';
+
+	let number = $state(0);
+</script>
+
+<UserInput userName = "Paven">
+    <h1>Dette er en test af component destructuring of props</h1>
+</UserInput>
+<!-- <UserInput /> -->
+
+<button
+    use:sound={{
+        src: hover_sound,
+        events: ['mouseover', 'mouseout'],
+        loop: true
+    }}
+	use:sound={{
+		src: click_sound,
+		events: ['click']
+	}}
+	onclick={() => number++}>Click me!</button
+>
+
+<style>
+	* {
+		font-size: 2rem;
+		display: flex;
+		justify-content: center;
+		/* text-align: center;
+        align-items: center;
+        align-content: center; */
+		margin: auto;
+		padding: 20px;
+	}
+	button {
+		padding: 4px;
+		border-radius: 8px;
+		border-width: 4px;
+		border-color: red;
+	}
+</style>
